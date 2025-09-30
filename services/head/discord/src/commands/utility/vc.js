a6dd91ee9985ec3bef8dec7c5a07f9ffea08eb82
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-09-30 18:37:20 +0200

    Moved `getOrCreateVoiceConnection` to separate file.

--- services/head/discord/src/commands/utility/vc.js
+++ services/head/discord/src/commands/utility/vc.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+const { getVoiceConnection } = require('@discordjs/voice');
+const { getOrCreateVoiceConnection } = require('./vc/VCConnection');
 
 
 module.exports = {
@@ -96,32 +97,5 @@ async function handleDisconnect(interaction) {
     } else{
         console.log("Not connected to a voice channel.")
         await interaction.editReply('❗ Not connected to a voice channel.');
-    }
-}
-
-function getOrCreateVoiceConnection(interaction, channel, selfDeaf = true, selfMute = true) {
-    const guildId = interaction.guild.id;
-    const voiceChannel = interaction.member.voice.channel;
-
-    try{
-        const connection = getVoiceConnection(guildId) ??
-            joinVoiceChannel({
-                channelId: voiceChannel.id,
-                guildId,
-                adapterCreator: interaction.guild.voiceAdapterCreator,
-                selfDeaf,
-                selfMute,
-            });
-
-        // Ensure the connection is set up correctly
-        connection.selfDeaf = selfDeaf;
-        connection.selfMute = selfMute;
-
-        console.log(`Connected to ${voiceChannel.name} in ${interaction.guild.name}`);
-        return connection;
-
-    } catch (error) {
-        console.error('Failed to connect to voice channel:', error);
-        return null;
     }
 }
